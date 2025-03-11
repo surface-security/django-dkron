c31617b57cf3621ca1fac378d6ab39b3b463fb11
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2025-03-11 12:22:26 +0200

    fix tests
    Signed-off-by: Bogdan Peter <bogdan.peter@paddypowerbetfair.com>

--- dkron/forms.py
+++ dkron/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from dkron.models import Job
 
 
 class JobForm(forms.ModelForm):
@@ -9,3 +10,7 @@ class JobForm(forms.ModelForm):
                 "Job schedule cannot start with * as this will schedule a job to start every second and can have unintended consequences."
             )
         return data
+
+    class Meta:
+        model = Job
+        fields = "__all__"
