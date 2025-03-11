2fd07f30fc0c0a03ee312cf5790db9b0d4b7a86f
Bogdan Peter <bogdan.peter@paddypowerbetfair.com>
2025-03-11 11:54:57 +0200

    black format
    Signed-off-by: Bogdan Peter <bogdan.peter@paddypowerbetfair.com>

--- dkron/forms.py
+++ dkron/forms.py
@@ -5,5 +5,7 @@ class JobForm(forms.ModelForm):
     def clean_schedule(self):
         data = self.cleaned_data["schedule"]
         if data.startswith("*"):
-            raise forms.ValidationError("Job schedule cannot start with * as this will schedule a job to start every second and can have unintended consequences.")
+            raise forms.ValidationError(
+                "Job schedule cannot start with * as this will schedule a job to start every second and can have unintended consequences."
+            )
         return data
