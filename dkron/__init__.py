ae82373520f9a59e48ea5064713d998a77f8a76f
Fábio Pinto <fabio.pinto@blip.pt>
2025-07-10 10:22:46 +0100

    fix: single quotes

--- dkron/__init__.py
+++ dkron/__init__.py
@@ -1,10 +1,10 @@
-__version__ = "1.2.2"
+__version__ = '1.2.2'
 
 # set default_app_config when using django earlier than 3.2
 try:
     import django
 
     if django.VERSION < (3, 2):
-        default_app_config = "dkron.apps.DkronConfig"
+        default_app_config = 'dkron.apps.DkronConfig'
 except ImportError:
     pass
